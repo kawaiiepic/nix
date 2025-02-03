63701a9cb2c5e8dafc271f5d349fe421a4757614
Mia <miathewiccancatgirl@gmail.com>
2025-02-03 17:25:45 -0500

    wyntor

--- home/hyprland/services/ags/config/widget/bar/Bluetooth.tsx
+++ home/hyprland/services/ags/config/widget/bar/Bluetooth.tsx
@@ -20,6 +20,6 @@ export default () => {
         self.visible = bluetooth.devices.length > 0;
       }),
 
-    visible: bind(bluetooth, "devices").as((devices) => devices.length > 0),
+    visible: bind(bluetooth, "isPowered"),
   });
 };
