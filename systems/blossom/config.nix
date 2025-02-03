d6d23f75b4819adfb59d84ae9a8218574d1a75e2
Mia <miathewiccancatgirl@gmail.com>
2025-02-03 15:31:29 -0500

    wyntor

---
+++ systems/blossom/config.nix
@@ -0,0 +1,34 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../../system/desktop/greetd.nix
+    ./hardware.nix
+    ../default.nix
+    ./storage.nix
+    
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+  ];
+
+  networking.hostName = "blossom";
+  
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
