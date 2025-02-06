bf73b4598eebe7b8ddcca94d842d39a3234d6442
Mia <miathewiccancatgirl@gmail.com>
2025-02-06 01:31:35 -0600

    wyntor

--- system/desktop/plasma.nix
+++ system/desktop/plasma.nix
@@ -1,4 +1,6 @@
-{
+{pkgs, ...}:{
   services.xserver.enable = true;
   services.desktopManager.plasma6.enable = true;
+  
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.seahorse.out}/libexec/seahorse/ssh-askpass";
 }
