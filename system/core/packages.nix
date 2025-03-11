cce0b3418d4865b6ce518abab0d452722d97fecf
Mia <miathewiccancatgirl@gmail.com>
2025-03-11 15:15:07 -0500

    efibootmgr git

--- system/core/packages.nix
+++ system/core/packages.nix
@@ -8,21 +8,23 @@
     wineWowPackages.staging
     toybox
     (pkgs.efibootmgr.overrideAttrs {
-      version = "boop";
+      version = "git";
       src = fetchFromGitHub {
-          owner = "rhboot";
-          repo = "efibootmgr";
-          tag = "16";
-          hash = "sha256-cRvM2gDd1zjTz/xIq7o+CHr/JxSTA1ssVdSs4DaCRyw=";
-        };
-        
-        patches = [
-            (fetchpatch {
-              name = "remove-extra-decl.patch";
-              url = "https://github.com/rhboot/efibootmgr/commit/99b578501643377e0b1994b2a068b790d189d5ad.patch";
-              sha256 = "1sbijvlpv4khkix3vix9mbhzffj8lp8zpnbxm9gnzjz8yssz9p5h";
-            })
-          ];
+        owner = "rhboot";
+        repo = "efibootmgr";
+        rev = "0a85e9baaac8a34e4a0bb9c23dfcc9c4f759e061";
+        hash = "sha256-XHfErh99UV68oC7gIfg5UIAB2L/X4Dj4EzqKu4QNpBw=";
+      };
+
+      patches = [
+
+      ];
+
+      makeFlags = [
+        "CFLAGS=-Wno-pointer-sign"
+        "EFIDIR=nixos"
+        "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+      ];
     })
   ];
 
