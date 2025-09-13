7202065c41588d293bbc8ee4aa7c76e4afb7fc00
Mia <miathewiccancatgirl@gmail.com>
2025-09-13 04:00:36 -0500

    update

--- system/core/network.nix
+++ system/core/network.nix
@@ -5,4 +5,10 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+  };
+
 }
