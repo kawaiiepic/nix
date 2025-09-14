6747dbe064a9efcb3e258a46040bc4e068e4d1c6
Mia <miathewiccancatgirl@gmail.com>
2025-09-14 07:14:14 -0500

    server

--- systems/dreamhouse/config.nix
+++ systems/dreamhouse/config.nix
@@ -8,15 +8,37 @@
 }:
 {
   imports = [
-    ../system/core/boot/cachyos.nix
-    ../system/core/boot.nix
-    ../system/core/packages.nix
-    ../system/core/shell.nix
-    ../system/core/users.nix
-    ../system/nix
+    # ../../system/desktop/greetd.nix
+    ./hardware.nix
+    ../default.nix
+    ./storage.nix
+    ../../system/desktop/greetd.nix
+    ../../system/desktop/niri.nix
+    
+    ../../modules/gpu-screen-recorder-ui.nix
+
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+  ];
+  
+  programs.gsr.enable = true;
+
+  networking.hostName = "dreamhouse";
+
+  hardware.opentabletdriver.enable = true;
+
+  environment.systemPackages = with pkgs; [
+
   ];
 
-  networking.hostName = "binarybarbie";
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   system.stateVersion = "23.11"; # Did you read the comment?
 }
